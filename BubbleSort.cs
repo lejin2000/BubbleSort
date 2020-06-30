77ad1279def7cdd93fdb2ec06e5923d81e2eaeaf
lejin2000 <lejin2000@hotmail.com>
2020-06-30 10:57:03 +0800

    Add project files.

---
+++ BubbleSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace BubbleSort
+{
+    internal class BubbleSort
+    {
+        private int[] _numbers;
+        private int intTemp;
+        private bool isSorted;
+
+        public int[] Numbers {
+            get {
+                return _numbers;
+            }
+            set {
+                _numbers = value;
+            } 
+        }
+     
+        public void sort()
+        {
+            for( var i = 0; i < _numbers.Length; i++)
+            {
+                isSorted = true;
+                for( var j = 1; j < _numbers.Length - i; j++)
+                {
+                    if (_numbers[j] < _numbers[j - 1])
+                    {
+                        swap(_numbers, j, j - 1);
+                        isSorted = false;
+                    }
+                }
+                if(isSorted)
+                {
+                    return;
+                }
+            }
+        }
+
+        public void swap(int[] array, int index1, int index2)
+        {
+            intTemp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = intTemp;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 2, 7,4,7,9,0};
+            var sorter = new BubbleSort();
+            sorter.Numbers = numbers;
+            sorter.sort(); 
+            Console.WriteLine("[{0}]", string.Join(", ", sorter.Numbers));
+        }
+    }
+}
